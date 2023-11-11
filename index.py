83d6ca005186e8a44db04442b5c292980b817a93
Bryan Silva <bryan.gps18@gmail.com>
2023-11-11 20:35:32 -0300

    modificando a estrutura principal

--- index.py
+++ index.py
@@ -1,50 +1,32 @@
 from random import randint
 from time import sleep
 from suporte import *
-cabeçalho()
-print('O ganhador, ganha um PC GAMER.')
 
+#programa principal
+cabeçalho()
 computador = randint(0, 10)
 premio = ['Celular', 'Tênis', 'Pc Gamer']
 sorteio = randint(0, 3)
+ok = False
 
-for c in range(0, 5):
-    jogador = int(input('Digite um numero (0 à 10): '))
-
-
-
-
-
-
-
-
-'''
-pessoas = 1
-contador = 0
-while pessoas != 5:
-    for cont in range(0, 4):
-        print(f'{pessoas} PARTECIPANTE.')
-        while True:
-            jogagor = int(input('Digite um número: '))
-            if (jogagor >= 0) and (jogagor <= 10):
-                break
-            print('Número invalido. Tente novamente')
-        contador += 1
-        
-        if jogagor == computador:
-            print('PARABÉNS, Você me venceu')
-            print(f'Você ganhou um {premio[sorteio]}')
-            print('-=' * 15)
-            break
+for contador in range(1, 5):
+    print(f'Tentativa - {contador} ')
+    jogador = verificação('Digite um numero (0 à 10):')
+    sleep(0.8)
+    if jogador == computador:
+        print(f'PARABÉNS, Você ganhou ', end='')
+        print(premio[sorteio])
+        premio.remove(f'{premio[sorteio]}')
+        if contador != 4:
+            resp = str(input('Quer continuar [S/N]: ')).upper()[0]
+            if resp in 'N':
+                ok = True
+    else:
+        if computador > jogador:
+            print('Você perdeu, tente novamente um numero MAIOR...')
         else:
-            if computador > jogagor:
-                print('Você perdeu, tente novamente um numero maior...')
-            else:
-                print('Você perdeu, tente novamente um numero menor...')
-        print('-=' * 20)
+            print('Você perdeu, tente novamente um numero MENOR..')
+    if ok:
+        break
 
-    pessoas += 1
-
-print(f'Tentativas {contador}')
-
-'''
+    print('-' * 25)
--- suporte.py
+++ suporte.py
@@ -3,3 +3,15 @@ def cabeçalho():
     print(f'TENTE ADIVINHAR O NÚMERO DE 0 À 10 QUE ESTOU PENSANDO...'.center(70))
     print('VOCÊ TEM 4 TENTATIVAS. BOA SORTE!!'.center(70))
     print('-' * 70)
+
+
+def verificação(msg):
+    while True:
+        jogador = int(input(f'{msg} '))
+        if 0 <= jogador <= 10:
+            break
+        else:
+            print('ERRO! Informe um valor entre 0 é 10')
+    return jogador
+
+
