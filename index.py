5cfae0e97991e677683e2af30b0db7d653e04a74
Bryan Silva <bryan.gps18@gmail.com>
2023-11-12 11:01:31 -0300

    adicionado uma função para saber a quantidade de participantes

--- index.py
+++ index.py
@@ -9,24 +9,32 @@ premio = ['Celular', 'Tênis', 'Pc Gamer']
 sorteio = randint(0, 3)
 ok = False
 
-for contador in range(1, 5):
-    print(f'Tentativa - {contador} ')
-    jogador = verificação('Digite um numero (0 à 10):')
-    sleep(0.8)
-    if jogador == computador:
-        print(f'PARABÉNS, Você ganhou ', end='')
-        print(premio[sorteio])
-        premio.remove(f'{premio[sorteio]}')
-        if contador != 4:
-            resp = str(input('Quer continuar [S/N]: ')).upper()[0]
-            if resp in 'N':
-                ok = True
-    else:
-        if computador > jogador:
-            print('Você perdeu, tente novamente um numero MAIOR...')
-        else:
-            print('Você perdeu, tente novamente um numero MENOR..')
-    if ok:
-        break
+pessoas = cadastro('Quantos participantes?')
+tot = len(pessoas)
+cont = 0
+while cont != tot:
+    print(f'Paricipante {pessoas[cont]["nome"]} tem 3 tentativas')
 
-    print('-' * 25)
+    for contador in range(1, 4):
+        print(f'Tentativa - {contador} ')
+        jogador = verificação('Digite um numero (0 à 10):')
+        sleep(0.8)
+        if jogador == computador:
+            print(f'PARABÉNS, Você ganhou ', end='')
+            print(premio[sorteio])
+            premio.remove(f'{premio[sorteio]}')
+            if contador != 4:
+                resp = str(input('Quer continuar [S/N]: ')).upper()[0]
+                if resp in 'N':
+                    ok = True
+        else:
+            if computador > jogador:
+                print(f'\033[33mVocê perdeu, tente novamente um numero {"\033[31mMAIOR\033[m"}...\033[m')
+            else:
+                print(f'\033[31mVocê perdeu, tente novamente um numero {"\033[31mMENOR\033[m"}...\033[m')
+        if ok:
+            break
+
+        print('-' * 25)
+    cont += 1
+
--- suporte.py
+++ suporte.py
@@ -1,7 +1,7 @@
 def cabeçalho():
     print('-' * 70)
     print(f'TENTE ADIVINHAR O NÚMERO DE 0 À 10 QUE ESTOU PENSANDO...'.center(70))
-    print('VOCÊ TEM 4 TENTATIVAS. BOA SORTE!!'.center(70))
+    print('VOCÊ TEM 3 TENTATIVAS. BOA SORTE!!'.center(70))
     print('-' * 70)
 
 
@@ -13,5 +13,18 @@ def verificação(msg):
         else:
             print('ERRO! Informe um valor entre 0 é 10')
     return jogador
+
+
+def cadastro(msg):
+    lista = list()
+    partici = dict()
+    quantidade = int(input(f'{msg} '))
+    for c in range(0, quantidade):
+        partici['nome'] = str(input('Nome: ')).title().strip()
+        partici['idade'] = int(input('Idade: '))
+        lista.append(partici.copy())
+        print('-' * 40)
+    return lista
+
 
 
