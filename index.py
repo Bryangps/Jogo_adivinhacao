9bc5cbcdbb96b554e97e6bbf9a7b950708109b2d
Bryan Silva <bryan.gps18@gmail.com>
2023-11-18 18:49:48 -0300

    foi terminado os tratamento de erro

--- index.py
+++ index.py
@@ -5,30 +5,26 @@ from suporte import *
 #programa principal
 cabecalho()
 qtd = quantidade('Quantos participantes?')
-if qtd != 0:
-    pessoas = cadastro(qtd)
-'''
+pessoas = cadastro(qtd)
+
 tot = len(pessoas)
 cont = 0
-
 while cont != tot:
     computador = randint(0, 10)
     print(f'Paricipante {pessoas[cont]["nome"]} tem 3 tentativas')
-    for contador in range(1, 4):
-        print(f'Tentativa - {contador} ')
+    for tente in range(1, 4):
+        print(f'Tentativa - {tente} ')
         jogador = verificacao('Digite um numero (0 à 10):')
         if jogador < 0:
             break
         sleep(0.8)
         if jogador != computador:
             if computador > jogador:
-                print(f'\033[33mVocê perdeu, tente novamente um numero {"\033[31mMAIOR\033[m"}...\033[m')
+                print(f'\033[31mVocê perdeu, tente novamente um numero {"\033[33mMAIOR\033[m"}...\033[m')
             else:
-                print(f'\033[31mVocê perdeu, tente novamente um numero {"\033[31mMENOR\033[m"}...\033[m')
+                print(f'\033[31mVocê perdeu, tente novamente um numero {"\033[33mMENOR\033[m"}...\033[m')
         else:
-            print(f'PARABÉNS!, Você ganhou.')
+            print(f'PARABÉNS {pessoas[cont]['nome']}!, Você ganhou.')
             break
-        print('-' * 25)
-    print('-=' * 18)
+        print('-' * 40)
     cont += 1
-'''
--- suporte.py
+++ suporte.py
@@ -5,22 +5,20 @@ def cabecalho():
     print('-' * 70)
 
 
-def verificacao(msg):
+def verificacao(num):
     while True:
         try:
-            jogador = int(input(f'{msg} '))
+            jogador = int(input(f'{num} '))
         except (ValueError, TypeError):
             print('Erro! Informe um numero interio')
         except KeyboardInterrupt:
             print('\nVolte sempre')
-            jogador = -1
-            break
+            return -1
         else:
             if 0 <= jogador <= 10:
-                break
+                return jogador
             else:
                 print('Erro! Escolha um numero de 0 à 10')
-    return jogador
 
 
 def quantidade(msg):
@@ -36,39 +34,47 @@ def quantidade(msg):
             return pessoa
 
 
+def nome():
+    while True:
+        try:
+            name = str(input('Nome: ')).title().strip()
+        except (ValueError, TypeError):
+            print('Erro! Valor digitado invalido, informe seu nome')
+        except KeyboardInterrupt:
+            print('\nVolte sempre')
+            return ''
+        else:
+            if not name:
+                print('O campo não pode estar vazio')
+            elif name.isdigit():
+                print('Ops! O campo não pode ser um número.')
+            else:
+                return name
+
+
+def idade():
+    while True:
+        try:
+            ide = int(input('Idade: '))
+        except (ValueError, TypeError):
+            print('Erro! Informe um numero interio')
+        except KeyboardInterrupt:
+            print('\nVolte sempre')
+            return ''
+        else:
+            return ide
+
+
 def cadastro(qtd):
     lista = list()
     partici = dict()
-    ok = False
     for c in range(0, qtd):
-        while True:
-            try:
-                partici['nome'] = str(input('Nome: ')).title().strip()
-            except (ValueError, TypeError):
-                print('Erro! Valor digitado invalido, informe seu nome')
-            except (KeyboardInterrupt, NameError):
-                print('\nVolte sempre')
-                ok = True
-                break
-            else:
-                if not partici['nome']:
-                    print('O campo não pode estar vazio')
-                elif partici['nome'].isdigit():
-                    print('Ops! O campo não pode ser um número.')
-                else:
-                    break
-
-        while not ok:
-            try:
-                partici['idade'] = int(input('Idade: '))
-            except (ValueError, TypeError):
-                print('Erro! Informe um numero interio')
-            except KeyboardInterrupt:
-                print('\nVolte sempre')
-            else:
-                lista.append(partici.copy())
-                print('-' * 40)
-                break
+        partici['nome'] = nome()
+        if partici['nome'] == '':
+            break
+        partici['idade'] = idade()
+        if partici['idade'] == '':
+            break
+        print('-' * 40)
+        lista.append(partici.copy())
     return lista
-
-
